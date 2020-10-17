b9703292bce051a23d578c050e2a92d689d3cedc
Nivas Ganesan <38051335+Nivas138@users.noreply.github.com>
2020-10-17 21:43:47 +0530

    SprintBOOT-REST Learning
    Creating SprintBoot-RESTful webservice sample project which holds Web , DevTools , JPA , H2 Dependencies to do the Spring MVC Projects including Apache Tomcat as embedded , To live reload with enhanced performance , persist data in SQL with Java Persistence API using Spring data , hibernate and h2 fastin-memory database using JDBC , R2DBC access respectively.

---
+++ firstmuyatchi.java
@@ -0,0 +1,35 @@
+package com.learningstudent.restwebservice.learingrestframework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * To make RestFul Webservice have to make this as Rest Controller
+ * 
+ * */
+
+@RestController
+public class firstmuyatchi {
+	
+	//Get Method - Rest Framework
+	//To Get the firstmuyartchistep1 result using GET HTTP Request Method in this Service
+	
+	@RequestMapping(method = RequestMethod.GET,path="/firstmuyartchi_inspring")
+	public String firstmuyatchiStep1 () {
+		return "Vetri Namathe ! Thuninthu Sel !";
+	}
+	
+	@GetMapping(path="/firstmuyartchi_inspring_step2")
+	public String firstmuyatchiStep2() {
+		return "Success lies in taking risks! Thuninthu Sel!";
+	}
+	
+	@GetMapping(path="/firstmuyartchi_inspring_step3_bean")
+	public FirstMuyartchiBean firstmuyatchiBean() {
+		return new FirstMuyartchiBean("Burn the Purspose to Grow ! Thuninthu Sel!");
+	}
+
+	
+}
