02d5c9495ea28158f4b7266af7262a5431b00d98
Nivas138 <nivasganesan97@gmail.com>
2020-10-27 13:52:14 +0530

    Create Bean with User Application

--- learningstudent/restwebservice/learingrestframework/LearingrestframeworkApplication.java
+++ learningstudent/restwebservice/learingrestframework/LearingrestframeworkApplication.java
@@ -2,8 +2,12 @@ package com.learningstudent.restwebservice.learingrestframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.store.users.UserResource;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = UserResource.class)
 public class LearingrestframeworkApplication {
 
 	public static void main(String[] args) {
--- store/users/UserDAOService.java
+++ store/users/UserDAOService.java
@@ -3,9 +3,15 @@ package com.store.users;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class UserDAOService {
 
 	private static List<User> userlist = new ArrayList<>();
+	
+	private static int userlistcount = 0;
 	
 	static {
 		userlist.add(new User("Nivas", 1,"Builder"));
@@ -13,9 +19,26 @@ public class UserDAOService {
 	}
 	
 	//function for find all
+	public List<User> findAll(){
+		return userlist;
+	}
 	
 	//function for put 
+	public User Save(User user) {
+		if(user.getId() == null) {
+			user.setId(++userlistcount);
+		}
+		userlist.add(user);
+		return null;
+	}
 	
 	//function for retrive 
-	
+	public User findSingle(int id) {
+		for (User user : userlist) {
+			if(id == user.getId()) {
+				return user;
+			}
+		}
+		return null;
+	}
 }
